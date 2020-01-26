f7d0c3b128a7708925d86413bb3a54ad01290265
VictorGus <retaow@gmail.com>
2020-01-27 00:27:52 +0300

    reorg repo

--- src/app/core.clj
+++ src/app/core.clj
@@ -4,21 +4,6 @@
             [app.auth])
   (:use org.httpkit.server))
 
-(defn client-info-handler [req]
-  (let [temperature (get-in req [:route-params :temperature])
-        blood-pressure (get-in req [:route-params :blood-pressure])
-        heart-rate (get-in req [:route-params :heart-rate])
-        opts (assoc {} :temperature temperature :blood-pressure blood-pressure :heart-rate heart-rate)]
-    (request-semantic! {:url "http://localhost:5556/" :opts opts})
-    {:status "200"
-     :headers {"Content-Type" "text/html"}
-     :body (:uid (:route-params req))}))
-
-(defn signup-handler [req]
-  {:status "200"
-   :headers {"Content-Type" "application/jsom"}
-   :body req})
-
 
 (def routes
   {:GET #'signup-handler
